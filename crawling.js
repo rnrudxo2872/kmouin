1e6c185d52cb0887ccd861c46e95124b324b2ea4
rnrudxo2872@naver.com <rnrudxo2872@naver.com>
2020-02-19 23:38:18 +0900

    nono

--- crawling.js
+++ crawling.js
@@ -1,4 +1,6 @@
 const request = require('request');
+var cheerio = require('cheerio')
+
 const options = {
     url : 'http://www.kmou.ac.kr/coop/dv/dietView/selectDietDateView.do?mi=1189',
     headers: {
@@ -7,8 +9,22 @@ const options = {
 }
 
 request(options, (error, response, html) => {
+
     if(error) throw error;
-    console.log(html);
+    let foodList = [];
+    $ = cheerio.load(html);
+    const $bodyList = $("div.BD_table").children("table.detail_tb");
+
+    $bodyList.each(function(i, elem) {
+      foodList[i] = {
+          title: $(this).find('thead tr th').text(),
+          date: $(this).find('tbody tr td.ta_c').text()
+      };
+    });
+    /*for(let i = 0 ; i < ulList.length; i ++){
+        ulList[i].date = ulList[i].date.replace(/\t/g,'')
+    }*/
+    console.log(foodList)
 })
 
 
